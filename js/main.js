471713ca8d4fde6c5d8c35e8f3d5934b39d9e0f1
Roman Roman <roman@cloudconsulting.no>
2018-12-11 19:42:34 +0200

    UPDATE

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@ const btn =  document.querySelector('.btn');
 const input = document.querySelector('.form-control');
 const resultEl = document.querySelector('.panel');
 let result;
+let gCal;
 const coefficient = 0.00086;
 
 // Rate in UAH from MikromegasServis per 1 Gcal;
-const rate = 1300;
+const rate = 1678.76;
 
 
 // Hide DOM result by default
@@ -27,13 +28,15 @@ input.addEventListener('input', function() {
 // Calc input
 const maceCalc =  function(value) {
   let convertToGcal = value * coefficient;
+  gCal = convertToGcal;
   result = Math.round(convertToGcal * rate);
   return result;
 };
 
 const showResult = function() {
   if(result > 0) {
-    document.querySelector('.panel-body li').innerHTML = '<strong>' + result + '</strong> грн';
+    document.querySelector('.panel-body p').innerHTML = '<strong>' + result + '</strong> грн' + 
+    '<br>' + '<strong>' + gCal + '</strong> Гкал';
     resultEl.style.display = 'block';
   }
 }
